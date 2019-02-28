f94916b4bcadedec97c3c04a92de65c18ff10fba
Tunde Yakub <tundenasri@gmail.com>
2019-02-28 21:12:38 +0100

    create action to add members to groups

---
+++ Ui/js/create-group.js
@@ -0,0 +1,23 @@
+let addMemberBtn = document.querySelector(".add-member");
+let listMember = document.querySelector(".list-members");
+let inputedMember = document.getElementById("member-input");
+
+
+
+const add = e => {
+    e.preventDefault();
+    if (inputedMember.value == "") {
+      return;
+    } else if (!inputedMember.value.includes("@")) {
+      console.log("string must include an @ to be an email");
+      return;
+    }
+    let allMembers = listMember;
+    let NewMember = document.createTextNode(inputedMember.value);
+    let span = document.createElement("p");
+    span.appendChild(NewMember);
+    allMembers.appendChild(span);
+    inputedMember.value = "";
+  };
+  
+  addMemberBtn.addEventListener("click", add);
