c725a07e8df590089c86f5dc35ddf92691aac626
Tunde Yakub <tundenasri@gmail.com>
2019-03-01 18:07:46 +0100

    create dialog box to display feedback

---
+++ Ui/js/sent-email.js
@@ -0,0 +1,33 @@
+const modal = document.getElementById("id-modal");
+const modalCloseButton = document.querySelector(".close-button");
+
+const toggleModal = () => {
+  modal.classList.toggle("show-modal");
+};
+
+const onClickWindow = event => {
+  if (event.target === modal) {
+    modal.classList.remove("show-modal");
+  }
+};
+
+const handleClick = event => {
+  let element = event.target;
+
+  while (element) {
+    if (
+      element.nodeName === "BUTTON" &&
+      /btn-retract/.test(element.className)
+    ) {
+      toggleModal();
+      break;
+    }
+
+    element = element.parentNode;
+    console.log(element);
+  }
+};
+
+document.addEventListener("click", handleClick);
+modalCloseButton.addEventListener("click", toggleModal);
+window.addEventListener("click", onClickWindow);
