3e72bf2114c55841b7337e414db3963e731629d5
Tunde Yakub <tundenasri@gmail.com>
2019-03-04 23:21:26 +0100

    write test for sign up route

--- server/test/userService.test.js
+++ server/test/userService.test.js
@@ -1,7 +1,10 @@
 import chai from 'chai';
+import chaiHttp from 'chai-http';
+
 import server from '../app';
 import UserService from '../services/userServices';
 
+chai.use(chaiHttp);
 const userServices = new UserService();
 const { expect } = chai;
 
@@ -21,5 +24,52 @@ describe('Test user signup services', () => {
     expect(newUser).to.have.property('email');
     expect(newUser).to.have.property('password');
     done();
+  });
+});
+
+describe('Test user signup route', () => {
+  it('should return 404 on wrong api call', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/auth/')
+      .end((err, res) => {
+        expect(res.status).to.eql(404);
+        done();
+      });
+  });
+  it('should return email already in use, if the email exists', (done) => {
+    const user = {
+      firstName: 'Tunde',
+      lastName: 'Nasri',
+      email: 'superuser@mail.com',
+      password: 'secret',
+    };
+    chai
+      .request(server)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        expect(res.body.status).to.eql(400);
+        expect(res.body).to.have.property('error');
+        done();
+      });
+  });
+  it('should post the user and return the user object, when correct details are passed along request', (done) => {
+    const user = {
+      firstName: 'Tunde',
+      lastName: 'Nasri',
+      email: 'tunde@mail.com',
+      password: 'secret',
+    };
+    chai.request(server)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .end((err, res) => {
+        expect(res.body.status).to.eql(201);
+        expect(res.body.data).to.be.an('object');
+        expect(res.body.data).to.have.property('token');
+        expect(res.body.data).to.have.property('name');
+        done();
+      });
   });
 });
