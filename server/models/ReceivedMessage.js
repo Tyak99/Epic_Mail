9803470712f31085b66eeb0eb6face14076aad14
Tunde Yakub <tundenasri@gmail.com>
2019-03-06 19:28:10 +0100

    return null when nullable data arent passed to message body

--- server/models/ReceivedMessage.js
+++ server/models/ReceivedMessage.js
@@ -2,6 +2,6 @@ export default class RecievedMessage {
   constructor() {
     this.receiverId = null;
     this.messageId = null;
-    this.createdOn = null;
+    this.createdOn = new Date();
   }
 }
