b1b50d7710bccdfdeecb1a199447a6508ed54abd
Tunde Yakub <tundenasri@gmail.com>
2019-03-06 12:54:16 +0100

    create message model

---
+++ server/models/Message.js
@@ -0,0 +1,12 @@
+export default class Message {
+  constructor() {
+    this.id = null;
+    this.createdOn = null;
+    this.subject = null;
+    this.message = null;
+    this.status = null;
+    this.senderId = null;
+    this.receiverId = null;
+    this.parentMessageId = null;
+  }
+}
