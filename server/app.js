ac1fa304bcef6037c71757be78b1184852ec2686
Tunde Yakub <tundenasri@gmail.com>
2019-03-08 22:00:32 +0100

    open port for heroku to use

--- server/app.js
+++ server/app.js
@@ -52,7 +52,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to epic mail');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('server has started');
 });
