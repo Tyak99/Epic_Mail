95bcb21acd1067b366eeb000e02e0dcbd1127d82
Tunde Yakub <tundenasri@gmail.com>
2019-03-08 20:46:18 +0100

    create documentation for message route

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -3,6 +3,153 @@ import messageController from '../controllers/messageControllers';
 
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * definitions:
+ *   Message:
+ *     properties:
+ *       subject:
+ *         type: string
+ *       message:
+ *         type: string
+ *       status:
+ *         type: string
+ *       createdOn:
+ *         type: string
+ *       senderId:
+ *         type: integer
+ *       receiverId:
+ *         type: integer
+ *       parentMessageId:
+ *         type: integer
+ */
+
+/**
+ * @swagger
+ * /api/v1/messages:
+ *   get:
+ *     summary: Get all received messages
+ *     description: Returns a list of user's received emails
+ *     tags:
+ *       - Messages
+ *     responses:
+ *       200:
+ *         description: An array of received messages
+ *         schema:
+ *           $ref: '#/definitions/Message'
+ */
+
+/**
+ * @swagger
+ * /api/v1/messages:
+ *   post:
+ *     tags:
+ *       - Message
+ *     description: Send an email to an individual
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: subject
+ *         description: message object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: message
+ *         description: message body
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: emailTo
+ *         description: the email of the message recipient
+ *         in: body
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ *       400:
+ *         description: Please input all the required data
+ *       404:
+ *         description: Email not found
+ */
+
+/**
+ * @swagger
+ * /api/v1/messages/sent:
+ *   get:
+ *     summary: Get all sent messages
+ *     description: Returns a list of user's sent emails
+ *     tags:
+ *       - Messages
+ *     responses:
+ *       200:
+ *         description: An array of sent messages
+ *         schema:
+ *           $ref: '#/definitions/Message'
+ */
+
+/**
+ * @swagger
+ * /api/v1/messages/unread:
+ *   get:
+ *     summary: Get all user's unread messages
+ *     description: Returns a list of user's unread emails
+ *     tags:
+ *       - Messages
+ *     responses:
+ *       200:
+ *         description: An array of unread messages
+ *         schema:
+ *           $ref: '#/definitions/Message'
+ */
+
+/**
+ * @swagger
+ * /api/v1/messages/{id}:
+ *   get:
+ *     tags:
+ *       - Message
+ *     description: Returns a single user's message
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Message's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: A single message
+ *         schema:
+ *           $ref: '#/definitions/Message'
+ */
+
+/**
+ * @swagger
+ * /api/v1/messages/{id}:
+ *   delete:
+ *     tags:
+ *       - Message
+ *     description: Deletes a single message
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Message's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Message Successfully deleted
+ *       400:
+ *         description: No message with that id found
+ *       404:
+ *         description: No id present to locate resource
+ */
+
 router.post('/', messageController.postMessage);
 router.get('/', messageController.getReceivedMessages);
 router.get('/sent', messageController.getSentMessages);
