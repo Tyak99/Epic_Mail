f2a1f81cb67bae650f25d8923d057d857f8905b0
Tunde Yakub <tundenasri@gmail.com>
2019-03-08 19:00:38 +0100

    write more test for post group service method

--- server/test/group.test.js
+++ server/test/group.test.js
@@ -7,9 +7,28 @@ const { expect } = chai;
 
 const groupServices = new GroupService();
 
+describe('Test all group service method', () => {
+  it('should return an array of group objects', (done) => {
+    const groups = groupServices.allGroups();
+    expect(groups).to.be.an('array');
+    groups.forEach((group) => {
+      expect(group).an.have.property('id');
+      expect(group).an.have.property('name');
+      expect(group)
+        .an.have.property('members')
+        .to.be.an('array');
+    });
+    done();
+  });
+});
 describe('Test create a group method', () => {
-  it('should return error if group name isnt specifid on creation', (done) => {
+  it('should return error if group no data is passed along the request to create group', (done) => {
     const createGroup = groupServices.postGroup();
+    expect(createGroup).to.eql('error');
+    done();
+  });
+  it('should return error if any required data isnt passed along request', (done) => {
+    const createGroup = groupServices.postGroup({ name: 'Team 2' });
     expect(createGroup).to.eql('error');
     done();
   });
