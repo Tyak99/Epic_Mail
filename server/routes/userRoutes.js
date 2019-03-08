baef23fc8cd2d701cb9fce7a921e85fd815e5cc5
Tunde Yakub <tundenasri@gmail.com>
2019-03-08 21:18:13 +0100

    write user routes documentation

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,82 @@ import userController from '../controllers/userControllers';
 
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * definitions:
+ *   SignUp:
+ *     properties:
+ *       email:
+ *         type: string
+ *       password:
+ *         type: string
+ *       firstName:
+ *         type: string
+ *       lastName:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   Login:
+ *     properties:
+ *       email:
+ *         type: string
+ *       password:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/signup:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     description: Sign up an account
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: user
+ *         description: user object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/SignUp'
+ *     responses:
+ *       201:
+ *         description: token
+ *       400:
+ *         description: Email already in use
+ *       404:
+ *         description: Email not found
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     description: Sign in an account
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: user
+ *         description: user object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Login'
+ *     responses:
+ *       200:
+ *         description: token
+ *       400:
+ *         description: Invalid email or password
+ */
+
+
 router.post('/signup', userController.signup);
 
 router.post('/login', userController.login);
