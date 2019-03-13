6c3404d5d050e7fb95f2e9e9f6754f484e47b932
Tunde Yakub <tundenasri@gmail.com>
2019-03-13 23:33:49 +0100

    create post group to database controller

--- server/controllers/groupControllers.js
+++ server/controllers/groupControllers.js
@@ -1,15 +1,38 @@
+import db from '../db/index';
 
 exports.postGroup = (req, res) => {
-  const { name, members } = req.body;
-  if (!name || !members) {
+  const { name } = req.body;
+  if (!name) {
     return res.send({
       status: 400,
-      error: 'Provide the neccessary details',
+      error: 'Please input group name',
     });
   }
-  const group = groupServices.postGroup(req.body);
-  return res.send({
-    status: 201,
-    data: group,
-  });
+  db.query(
+    'INSERT INTO groups (name) VALUES ($1) RETURNING *',
+    [name],
+    (err, response) => {
+      if (err) {
+        return err;
+      }
+      const { id, name } = response.rows[0];
+      db.query(
+        'INSERT INTO groupmembers (groupid, memberid, userrole) VALUES ($1, $2, $3) RETURNING *',
+        [id, 2, 'admin'],
+        (err, data) => {
+          if (err) {
+            return err;
+          }
+          return res.send({
+            status: 201,
+            data: {
+              id,
+              name,
+              role: data.rows[0].userrole,
+            },
+          });
+        }
+      );
+    }
+  );
 };
