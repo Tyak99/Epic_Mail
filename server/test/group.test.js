81ff67b7fe8f85ea9c0884980518351137b2b8c8
Tunde Yakub <tundenasri@gmail.com>
2019-03-13 21:48:51 +0100

    edit create group test

--- server/test/group.test.js
+++ server/test/group.test.js
@@ -1,58 +1,17 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../app';
-import GroupService from '../services/groupServices';
 
 const { expect } = chai;
 chai.use(chaiHttp);
 
-const groupServices = new GroupService();
-
-describe('Test all group service method', () => {
-  it('should return an array of group objects', (done) => {
-    const groups = groupServices.allGroups();
-    expect(groups).to.be.an('array');
-    groups.forEach((group) => {
-      expect(group).an.have.property('id');
-      expect(group).an.have.property('name');
-      expect(group)
-        .an.have.property('members')
-        .to.be.an('array');
-    });
-    done();
-  });
-});
 
 describe('Test create group', () => {
-  describe('Test create a group method', () => {
-    it('should return error if group no data is passed along the request to create group', (done) => {
-      const createGroup = groupServices.postGroup();
-      expect(createGroup).to.eql('error');
-      done();
-    });
-    it('should return error if any required data isnt passed along request', (done) => {
-      const createGroup = groupServices.postGroup({ name: 'Team 2' });
-      expect(createGroup).to.eql('error');
-      done();
-    });
-    it('should return an object of the created group details', (done) => {
-      const group = {
-        name: 'Akatsuki',
-        members: ['superuser@mail.com', 'tunde@mail.com'],
-      };
-      const createdGroup = groupServices.postGroup(group);
-      expect(createdGroup).to.be.an('object');
-      expect(createdGroup).an.have.property('id');
-      expect(createdGroup).an.have.property('name');
-      done();
-    });
-  });
-
   describe('Test create a group route', () => {
     it('should return 404 on wrong api call', (done) => {
       chai
         .request(server)
-        .post('/api/v1/wrongapi')
+        .post('/api/v2/wrongapi')
         .end((err, res) => {
           expect(res.status).to.eql(404);
           done();
@@ -61,27 +20,25 @@ describe('Test create group', () => {
     it('should return error when needed details arent passed along', (done) => {
       chai
         .request(server)
-        .post('/api/v1/groups')
+        .post('/api/v2/groups')
         .send({})
         .end((err, res) => {
           expect(res.body.status).an.eql(400);
           expect(res.body).to.have.property('error');
+          expect(res.body.error).to.eql('Please input group name');
           done();
         });
     });
     it('should respond with the created group object if its created successfully', (done) => {
       chai
         .request(server)
-        .post('/api/v1/groups')
-        .send({ name: 'Team 1', members: ['name@mail.com', 'hope@mail.com'] })
+        .post('/api/v2/groups')
+        .send({ name: 'Team 1'})
         .end((err, res) => {
           expect(res.body.status).to.eql(201);
           expect(res.body.data).to.be.an('object');
           expect(res.body.data).to.have.property('id');
           expect(res.body.data).to.have.property('name');
-          expect(res.body.data)
-            .to.have.property('members')
-            .to.be.an('array');
           done();
         });
     });
