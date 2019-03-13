2de19c10d3d2513873c23aff01be324e14e6e832
Tunde Yakub <tundenasri@gmail.com>
2019-03-13 15:29:24 +0100

    set up different database for tests

--- server/db/index.js
+++ server/db/index.js
@@ -2,9 +2,12 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
+let db = process.env.DATABASE_URL;
+if (process.env.NODE_ENV == 'test') {
+  db = process.env.TEST_DATABASE_URL;
+}
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: db,
 });
 
 module.exports = {
