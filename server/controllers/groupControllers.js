ed8f539fdaf33279899223b0029fcdda95c59ae3
Tunde Yakub <tundenasri@gmail.com>
2019-03-14 15:28:15 +0100

    refactor code to make test pass

--- server/controllers/groupControllers.js
+++ server/controllers/groupControllers.js
@@ -17,38 +17,33 @@ exports.postGroup = (req, res) => {
     });
   }
   db.query('SELECT * FROM groups WHERE name = $1', [name], (err, group) => {
+    console.log(group);
     if (group.rows[0]) {
       return res.send({
         status: 400,
         error: 'Group name already taken, try another',
       });
     }
-  });
-  db.query(
-    'INSERT INTO groups (name) VALUES ($1) RETURNING *',
-    [name],
-    (err, response) => {
-      if (err) {
-        return err;
-      }
-      const { id, name } = response.rows[0];
-      db.query(
-        'INSERT INTO groupmembers (groupid, memberid, userrole) VALUES ($1, $2, $3) RETURNING *',
-        [id, 2, 'admin'],
-        (err, data) => {
-          if (err) {
-            return err;
+    db.query(
+      'INSERT INTO groups (name) VALUES ($1) RETURNING *',
+      [name],
+      (err, response) => {
+        const { id, name } = response.rows[0];
+        db.query(
+          'INSERT INTO groupmembers (groupid, memberid, userrole) VALUES ($1, $2, $3) RETURNING *',
+          [id, 2, 'admin'],
+          (err, data) => {
+            return res.send({
+              status: 201,
+              data: {
+                id,
+                name,
+                role: data.rows[0].userrole,
+              },
+            });
           }
-          return res.send({
-            status: 201,
-            data: {
-              id,
-              name,
-              role: data.rows[0].userrole,
-            },
-          });
-        }
-      );
-    }
-  );
+        );
+      }
+    );
+  });
 };
--- server/test/group.test.js
+++ server/test/group.test.js
@@ -7,102 +7,103 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 before((done) => {
+  db.query('DROP TABLE IF EXISTS groupmembers', (err, res) => {
+    console.log('DROP TABLE GROUPMEMBERS');
+
+    done();
+  });
+});
+
+before((done) => {
   db.query('DROP TABLE IF EXISTS groups', (err, res) => {
     done();
   });
 });
+
 before((done) => {
   db.query(
     `CREATE TABLE groups (
     id serial PRIMARY KEY,
-    name varchar(255) NOT NULL
+    name varchar(255) NOT NULL UNIQUE
 )`,
     (err, res) => {
-      done();
-    }
-  );
-});
-before((done) => {
-  db.query('DROP TABLE IF EXISTS groupmembers', (err, res) => {
-    done();
-  });
-});
-before((done) => {
-  db.query(
-    `CREATE TABLE groupmembers (
-    groupid INT REFERENCES groups(id),
-    memberid INT NOT NULL,
-    userrole varchar(255) NOT NULL
-)`,
-    (err, res) => {
+      console.log('CREATE TABLE GROUPS');
       done();
     }
   );
 });
 
-describe('Test create group', () => {
-  describe('Test create a group route', () => {
-    it('should return 404 on wrong api call', (done) => {
-      chai
-        .request(server)
-        .post('/api/v1/wrongapi')
-        .end((err, res) => {
-          expect(res.status).to.eql(404);
-          done();
-        });
-    });
-    it('should return error when needed details arent passed along', (done) => {
-      chai
-        .request(server)
-        .post('/api/v1/groups')
-        .send({})
-        .end((err, res) => {
-          expect(res.body.status).an.eql(400);
-          expect(res.body).to.have.property('error');
-          expect(res.body.error).to.eql('Please input group name');
-          done();
-        });
-    });
-    it('should return error when group name input is less than 4', (done) => {
-      chai
-        .request(server)
-        .post('/api/v1/groups')
-        .send({ name: 'Te' })
-        .end((err, res) => {
-          expect(res.body.status).an.eql(400);
-          expect(res.body).to.have.property('error');
-          expect(res.body.error).to.eql(
-            'Group name must be upto 4 letters long and contain only letters and numbers'
-          );
-          done();
-        });
-    });
-    it('should respond with the created group object if its created successfully', (done) => {
-      chai
-        .request(server)
-        .post('/api/v1/groups')
-        .send({ name: 'Team1' })
-        .end((err, res) => {
-          expect(res.body.status).to.eql(201);
-          expect(res.body.data).to.be.an('object');
-          expect(res.body.data).to.have.property('id');
-          expect(res.body.data).to.have.property('name');
-          done();
-        });
-    });
-    it('should return error when group name already exists in the database', (done) => {
-      chai
-        .request(server)
-        .post('/api/v1/groups')
-        .send({ name: 'Team1' })
-        .end((err, res) => {
-          expect(res.body.status).an.eql(400);
-          expect(res.body).to.have.property('error');
-          expect(res.body.error).to.eql(
-            'Group name already taken, try another'
-          );
-          done();
-        });
-    });
+before((done) => {
+  db.query(
+    `CREATE TABLE groupmembers (
+    groupid INT REFERENCES groups(id) ON DELETE CASCADE,
+    memberid INT NOT NULL,
+    userrole varchar(255) NOT NULL
+)`,
+    (err, res) => {
+      console.log('CREATE TABLE GROUPMEMBERS');
+      done();
+    }
+  );
+});
+
+describe('Test create a group route', () => {
+  it('should return 404 on wrong api call', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/wrongapi')
+      .end((err, res) => {
+        expect(res.status).to.eql(404);
+        done();
+      });
+  });
+  it('should return error when needed details arent passed along', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/groups')
+      .send({})
+      .end((err, res) => {
+        expect(res.body.status).an.eql(400);
+        expect(res.body).to.have.property('error');
+        expect(res.body.error).to.eql('Please input group name');
+        done();
+      });
+  });
+  it('should return error when group name input is less than 4', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/groups')
+      .send({ name: 'Te' })
+      .end((err, res) => {
+        expect(res.body.status).an.eql(400);
+        expect(res.body).to.have.property('error');
+        expect(res.body.error).to.eql(
+          'Group name must be upto 4 letters long and contain only letters and numbers'
+        );
+        done();
+      });
+  });
+  it('should respond with the created group object if its created successfully', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/groups')
+      .send({ name: 'team1' })
+      .end((err, res) => {
+        expect(res.body.status).to.eql(201);
+        expect(res.body.data).to.be.an('object');
+        expect(res.body.data).to.have.property('id');
+        expect(res.body.data).to.have.property('name');
+        done();
+      });
+  });
+  it('should return error when group name aready exists in database', (done) => {
+    chai
+      .request(server)
+      .post('/api/v1/groups')
+      .send({ name: 'team1' })
+      .end((err, res) => {
+        expect(res.body.status).to.eql(400);
+        done();
+      });
   });
 });
