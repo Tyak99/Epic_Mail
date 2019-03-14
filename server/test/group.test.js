fd5b42972d2dfb909a67286041286716c578a2ab
Tunde Yakub <tundenasri@gmail.com>
2019-03-14 12:34:50 +0100

    add more test for create group route

--- server/test/group.test.js
+++ server/test/group.test.js
@@ -1,10 +1,44 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../app';
+import db from '../db/index';
 
 const { expect } = chai;
 chai.use(chaiHttp);
 
+before((done) => {
+  db.query('DROP TABLE IF EXISTS groups', (err, res) => {
+    done();
+  });
+});
+before((done) => {
+  db.query(
+    `CREATE TABLE groups (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL
+)`,
+    (err, res) => {
+      done();
+    }
+  );
+});
+before((done) => {
+  db.query('DROP TABLE IF EXISTS groupmembers', (err, res) => {
+    done();
+  });
+});
+before((done) => {
+  db.query(
+    `CREATE TABLE groupmembers (
+    groupid INT REFERENCES groups(id),
+    memberid INT NOT NULL,
+    userrole varchar(255) NOT NULL
+)`,
+    (err, res) => {
+      done();
+    }
+  );
+});
 
 describe('Test create group', () => {
   describe('Test create a group route', () => {
@@ -29,16 +63,44 @@ describe('Test create group', () => {
           done();
         });
     });
+    it('should return error when group name input is less than 4', (done) => {
+      chai
+        .request(server)
+        .post('/api/v1/groups')
+        .send({ name: 'Te' })
+        .end((err, res) => {
+          expect(res.body.status).an.eql(400);
+          expect(res.body).to.have.property('error');
+          expect(res.body.error).to.eql(
+            'Group name must be upto 4 letters long and contain only letters and numbers'
+          );
+          done();
+        });
+    });
     it('should respond with the created group object if its created successfully', (done) => {
       chai
         .request(server)
         .post('/api/v1/groups')
-        .send({ name: 'Team 1'})
+        .send({ name: 'Team1' })
         .end((err, res) => {
           expect(res.body.status).to.eql(201);
           expect(res.body.data).to.be.an('object');
           expect(res.body.data).to.have.property('id');
           expect(res.body.data).to.have.property('name');
+          done();
+        });
+    });
+    it('should return error when group name already exists in the database', (done) => {
+      chai
+        .request(server)
+        .post('/api/v1/groups')
+        .send({ name: 'Team1' })
+        .end((err, res) => {
+          expect(res.body.status).an.eql(400);
+          expect(res.body).to.have.property('error');
+          expect(res.body.error).to.eql(
+            'Group name already taken, try another'
+          );
           done();
         });
     });
