76bc5b6c57fe9705c925af550cc65ea2443ca7aa
Tunde Yakub <tundenasri@gmail.com>
2019-03-15 16:01:57 +0100

    remove unnecessary getReceived and getSent messages method

--- server/services/messageServices.js
+++ server/services/messageServices.js
@@ -1,7 +1,5 @@
 /* eslint-disable class-methods-use-this */
 import Message from '../models/Message';
-import ReceivedMessage from '../models/ReceivedMessage';
-import SentMessage from '../models/SentMessages';
 import UserService from './userServices';
 
 const userServices = new UserService();
@@ -71,62 +69,6 @@ export default class MessageService {
     });
   }
 
-  AllReceivedMessage() {
-    this.receivedMessages = [
-      {
-        receiverId: 2,
-        messageId: 1,
-        createdOn: 1551886333846,
-      },
-    ];
-    return this.receivedMessages.map((message) => {
-      const newReceivedMessage = new ReceivedMessage();
-      newReceivedMessage.receiverId = message.receiverId;
-      newReceivedMessage.messageId = message.messageId;
-      newReceivedMessage.createdOn = message.createdOn;
-      return newReceivedMessage;
-    });
-  }
-
-  AllSentMessages() {
-    this.sentMessages = [
-      {
-        senderId: 1,
-        messageId: 1,
-        createdOn: 1551886333846,
-      },
-    ];
-    return this.sentMessages.map((message) => {
-      const newSentMessage = new SentMessage();
-      newSentMessage.receiverId = message.senderId;
-      newSentMessage.messageId = message.messageId;
-      newSentMessage.createdOn = message.createdOn;
-      return newSentMessage;
-    });
-  }
-
-  postReceivedMessage(data) {
-    const { receiverId } = data;
-    if (!receiverId) {
-      return 'error';
-    }
-    const message = new ReceivedMessage();
-    message.receiverId = data.receiverId;
-    message.messageId = data.messageId;
-    return message;
-  }
-
-  postSentMessage(data) {
-    const { senderId, messageId } = data;
-    if (!senderId) {
-      return 'error';
-    }
-    const message = new SentMessage();
-    message.senderId = senderId;
-    message.messageId = messageId;
-    return message;
-  }
-
   getReceivedMessage() {
     const allMessage = this.AllMessage();
     return allMessage.filter((message) => message.receiverId === 1);
@@ -170,18 +112,6 @@ export default class MessageService {
     newMessage.receiverId = toWHo === null ? null : toWHo.id;
     newMessage.parentMessageId = data.parentMessageId || null;
 
-    if (newMessage.receiverId !== null) {
-      this.postReceivedMessage({
-        receiverId: newMessage.receiverId,
-        messageId: newMessage.id,
-      });
-    }
-    if (newMessage.senderId !== null) {
-      this.postSentMessage({
-        senderId: newMessage.senderId,
-        messageId: newMessage.id,
-      });
-    }
     this.messages.push(newMessage);
     return newMessage;
   }
