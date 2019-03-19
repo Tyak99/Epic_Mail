860360988b75b6e0e038857d483e878bd99c6392
Tunde Yakub <tundenasri@gmail.com>
2019-03-19 19:02:51 +0100

    define insert into groupmembers table to add user as group admin

--- server/controllers/groupControllers.js
+++ server/controllers/groupControllers.js
@@ -19,14 +19,17 @@ exports.postGroup = (req, res) => {
       'INSERT INTO groups (name, adminid) VALUES ($1, $2) RETURNING *',
       [name, req.decoded.sub],
       (err, group) => {
-        return res.status(201).json({
-          status: 'success',
-          data: {
-            id: group.rows[0].id,
-            name: group.rows[0].name,
-            role: 'admin',
-          }
-        });
+        const { id } = group.rows[0]
+        db.query('INSERT INTO groupmembers (groupid, memberid, userrole) VALUES ($1, $2, $3) RETURNING *', [id, req.decoded.sub, 'admin'], (err, groupmember) => {
+          return res.status(201).json({
+            status: 'success',
+            data: {
+              id: group.rows[0].id,
+              name: group.rows[0].name,
+              role: groupmember.rows[0].userrole,
+            }
+          });
+        })
       }
     );
   });
