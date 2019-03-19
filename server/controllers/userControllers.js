71693a4231adaa31523b47ab5a67ae9d9f0ac354
Tunde Yakub <tundenasri@gmail.com>
2019-03-19 11:17:00 +0100

    define controller for user sign up route

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,6 @@
 import { validationResult } from 'express-validator/check';
 import tokenFunction from '../utils/tokenHandler';
+import db from '../database/index';
 
 
 exports.signup = (req, res) => {
@@ -10,6 +11,33 @@ exports.signup = (req, res) => {
       error: errors.array()[0].msg,
     });
   }
+  // first check the database if that email exists previously
+  db.query('SELECT * FROM users WHERE email = $1', [req.body.email], (err, user) => {
+    if (user.rows[0]) {
+      return res.status(404).json({
+        status: 'failed',
+        error: 'User with this email already exists'
+      })
+    }
+    // if it doesnt then create the user
+    const { email, password, firstName, lastName } = req.body;
+    const values = [email, password, firstName, lastName];
+    db.query('INSERT INTO users (email, password, firstname, lastname) VALUES ($1, $2, $3, $4) RETURNING *', values, (err, createdUser) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'failed',
+          error: 'Internal server error',
+        });
+      }
+      return res.status(201).json({
+        status: 'success',
+        data: {
+          name: createdUser.rows[0].firstname,
+          token: tokenFunction(req.body)
+        },
+      });
+    });
+  });
 };
 
 exports.login = (req, res) => {
