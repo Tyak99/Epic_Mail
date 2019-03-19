83ddb6b75d4ba5ca7e41a104a495b17ae6de0bcf
Tunde Yakub <tundenasri@gmail.com>
2019-03-19 18:50:22 +0100

    define create a group controller

--- server/controllers/groupControllers.js
+++ server/controllers/groupControllers.js
@@ -1,10 +1,33 @@
+import db from '../database/index';
 
 exports.postGroup = (req, res) => {
-  const { name, members } = req.body;
-  if (!name || !members) {
-    return res.send({
-      status: 400,
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({
+      status: 'Failed',
       error: 'Provide the neccessary details',
     });
   }
+  db.query('SELECT * FROM groups WHERE name = $1', [name], (err, data) => {
+    if (data.rows[0]) {
+      return res.status(409).json({
+        status: 'Failed',
+        error: 'Team name already exist',
+      });
+    }
+    db.query(
+      'INSERT INTO groups (name, adminid) VALUES ($1, $2) RETURNING *',
+      [name, req.decoded.sub],
+      (err, group) => {
+        return res.status(201).json({
+          status: 'success',
+          data: {
+            id: group.rows[0].id,
+            name: group.rows[0].name,
+            role: 'admin',
+          }
+        });
+      }
+    );
+  });
 };
