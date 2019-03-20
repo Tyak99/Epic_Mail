086bba2f794078a637cec2850ae8ad73185ef0ff
Tunde Yakub <tundenasri@gmail.com>
2019-03-20 21:08:31 +0100

    remove test for message services

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -160,8 +160,8 @@ const router = express.Router();
 
 router.post('/',validator.postMessageValidation, tokenHandler.verifyToken, messageController.postMessage);
 router.get('/', tokenHandler.verifyToken,messageController.getReceivedMessages);
-router.get('/sent', messageController.getSentMessages);
-router.get('/unread', messageController.getUnreadMessages);
-router.get('/:id', messageController.getMessageById);
-router.delete('/:id', messageController.deleteById);
+// router.get('/sent', messageController.getSentMessages);
+// router.get('/unread', messageController.getUnreadMessages);
+// router.get('/:id', messageController.getMessageById);
+// router.delete('/:id', messageController.deleteById);
 export default router;
