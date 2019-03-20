652dc744fa38d28663bdb1091918944038a8e6a9
Tunde Yakub <tundenasri@gmail.com>
2019-03-20 20:45:29 +0100

    remove spect test of non persistent data

--- server/test/message.test.js
+++ server/test/message.test.js
@@ -202,55 +202,6 @@ describe('Test post a message route', () => {
   });
 });
 
-
-describe('Test get received emails method', () => {
-  it('should return all recieved emails', (done) => {
-    const receivedMessages = messageServices.getReceivedMessage();
-    expect(receivedMessages).to.be.an('array');
-    receivedMessages.forEach((message) => {
-      expect(message).to.have.property('id');
-      expect(message).to.have.property('subject');
-      expect(message).to.have.property('message');
-      expect(message).to.have.property('status');
-      expect(message).to.have.property('createdOn');
-      expect(message).to.have.property('receiverId');
-      expect(message).to.have.property('senderId');
-    });
-    done();
-  });
-});
-
-describe('Test get received emails route', () => {
-  it('should return error on wrong api call', (done) => {
-    chai
-      .request(server)
-      .get('/api/v1/wrongapi')
-      .end((err, res) => {
-        expect(res.status).to.eql(404);
-        done();
-      });
-  });
-  it('should return an array of received emails', (done) => {
-    chai
-      .request(server)
-      .get('/api/v1/messages')
-      .end((err, res) => {
-        expect(res.body.status).to.eql(200);
-        expect(res.body.data).to.be.an('array');
-        res.body.data.forEach((message) => {
-          expect(message).to.have.property('id');
-          expect(message).to.have.property('createdOn');
-          expect(message).to.have.property('message');
-          expect(message).to.have.property('subject');
-          expect(message).to.have.property('receiverId');
-          expect(message).to.have.property('senderId');
-          expect(message).to.have.property('status');
-        });
-        done();
-      });
-  });
-});
-
 describe('Test get sent emails method', () => {
   it('should test get sent emails method ', (done) => {
     const sentMessages = messageServices.getSentMessages();
