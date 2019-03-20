00d62ef76ee92c38062a7d774cca74ca582aed7d
Tunde Yakub <tundenasri@gmail.com>
2019-03-20 12:23:41 +0100

    UPDATE THE migration

--- server/migration/index.js
+++ server/migration/index.js
@@ -3,11 +3,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-let db = process.env.TEST_DATABASE_URL;
+let db = process.env.DATABASE_URL;
 if (process.env.NODE_ENV == 'test') {
   db = process.env.TEST_DATABASE_URL;
 }
-
 const pool = new Pool({
   connectionString: db,
 });
