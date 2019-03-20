7bbe4486109573a0062c494b3fbbb41ef6f66234
Tunde Yakub <tundenasri@gmail.com>
2019-03-20 13:55:36 +0100

    fix cascade issue to allow group to be deleted by user

--- server/controllers/groupControllers.js
+++ server/controllers/groupControllers.js
@@ -44,7 +44,7 @@ const addUserToGroup = (req, res) => {
   const userId = req.decoded.sub;
   // search the group table if such group exists
   db.query('SELECT * FROM groups WHERE id = $1', [groupid], (err, group) => {
-    if (!group) {
+    if (!group.rows[0]) {
       return res.status(404).json({
         status: 'Failed',
         error: 'Group does not exist',
