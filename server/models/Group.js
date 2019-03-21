3603545510de297baf1ef9ebca172afc50883924
Tunde Yakub <tundenasri@gmail.com>
2019-03-21 10:20:43 +0100

    remove non persistent data models

--- server/models/Group.js
+++
@@ -1,7 +0,0 @@
-export default class Group {
-  constructor() {
-    this.id = null;
-    this.name = null;
-    this.members = [];
-  }
-}
--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-export default class User {
-  constructor() {
-    this.id = null;
-    this.email = null;
-    this.password = null;
-    this.firstName = null;
-    this.lastName = null;
-  }
-}
