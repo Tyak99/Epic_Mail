0a5450207c539c134e0d69786a74061956b1dd35
Tunde Yakub <tundenasri@gmail.com>
2019-03-21 04:54:53 +0100

    update message test

--- server/test/message.test.js
+++ server/test/message.test.js
@@ -393,3 +393,59 @@ describe('Test DELETE message by id route', () => {
       });
   });
 });
+
+describe('Test errors returned when database is down', () => {
+  before((done) => {
+    db.query('DROP TABLE IF EXISTS messages CASCADE', (err, res) => {
+      done();
+    });
+  });
+  it('should test for error on GET MESSAGE by id when database is down', (done) => {
+    chai
+      .request(server)
+      .get('/api/v1/messages/1')
+      .set('Authorization', userToken)
+      .end((err, res) => {
+        expect(res.status).to.eql(500);
+        expect(res.body)
+          .to.have.property('status')
+          .to.eql('failed');
+        expect(res.body)
+          .to.have.property('error')
+          .to.eql('Internal server error');
+        done();
+      });
+  });
+  it('should test for error on get unread message route when database is down', (done) => {
+    chai
+      .request(server)
+      .get('/api/v1/messages/unread')
+      .set('Authorization', secondToken)
+      .end((err, res) => {
+        expect(res.status).to.eql(500);
+        expect(res.body)
+          .to.have.property('status')
+          .to.eql('failed');
+        expect(res.body)
+          .to.have.property('error')
+          .to.eql('Internal server error');
+        done();
+      });
+  });
+  it('should test for error on delete message by id route when server is down', (done) => {
+    chai
+      .request(server)
+      .delete('/api/v1/messages/1')
+      .set('Authorization', secondToken)
+      .end((err, res) => {
+        expect(res.status).to.eql(500);
+        expect(res.body)
+          .to.have.property('status')
+          .to.eql('failed');
+        expect(res.body)
+          .to.have.property('error')
+          .to.eql('Internal server error');
+        done();
+      });
+  });
+});
