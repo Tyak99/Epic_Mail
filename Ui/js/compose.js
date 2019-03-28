638a2e4db4d13d1f12f137d88705278d74064c97
Tunde Yakub <tundenasri@gmail.com>
2019-03-28 15:33:23 +0100

    define save message as draft function

--- Ui/js/compose.js
+++ Ui/js/compose.js
@@ -1,17 +1,44 @@
-let modal = document.getElementById("id-modal");
-let sendButton = document.querySelector(".btn-send");
-let closeModalButton = document.querySelector(".close-button");
-let okayModalButton = document.querySelector(".yes-modal")
-let emailTo = document.querySelector(".email-to");
+const modal = document.getElementById('id-modal');
+const sendButton = document.querySelector('.btn-send');
+const draftButton = document.querySelector('.btn-draft');
+const closeModalButton = document.querySelector('.close-button');
+const okayModalButton = document.querySelector('.yes-modal');
+const emailTo = document.querySelector('.email-to');
+const messageBody = document.getElementById('message-body');
+const messageSubject = document.getElementById('message-subject');
 
 function toggleModal(e) {
-  if (emailTo.value == "") {
+  if (emailTo.value == '') {
     e.preventDefault();
-    modal.classList.toggle("show-modal");
+    modal.classList.toggle('show-modal');
   }
 }
+const saveDraft = (e) => {
+  e.preventDefault();
+  const data = JSON.stringify({
+    subject: messageSubject.value,
+    message: messageBody.value,
+  });
+  const url = 'http://localhost:3000/api/v1/messages';
+
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  headers.append('authorization', localStorage.getItem('token'));
+
+  fetch(url, {
+    method: 'POST',
+    body: data,
+    headers,
+  })
+    .then((response) => response.json())
+    .then((res) => console.log(res))
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 
-sendButton.addEventListener("click", toggleModal);
-closeModalButton.addEventListener("click", toggleModal);
-okayModalButton.addEventListener('click', toggleModal)
+sendButton.addEventListener('click', toggleModal);
+draftButton.addEventListener('click', saveDraft);
+closeModalButton.addEventListener('click', toggleModal);
+okayModalButton.addEventListener('click', toggleModal);
