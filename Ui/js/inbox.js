c2a9f630eec5d207cfceb13f5ef0fd701e3cec93
Tunde Yakub <tundenasri@gmail.com>
2019-04-10 12:07:01 +0100

    fetch inbox to ui page

--- Ui/js/inbox.js
+++ Ui/js/inbox.js
@@ -15,8 +15,29 @@ fetch(url, {
     if (data.data.message) {
       const h2 = document.createElement('h2');
       h2.setAttribute('class', 'empty-inbox');
-      h2.textContent = ('No Received Messages');
+      h2.textContent = 'No Received Messages';
       mainSection.appendChild(h2);
     }
+    const ul = document.createElement('ul');
+    console.log(data.data);
+    data.data.forEach((message) => {
+      const li = document.createElement('li');
+      const span1 = document.createElement('span');
+      const span2 = document.createElement('span');
+      const span3 = document.createElement('span');
+      span1.setAttribute('class', 'sender');
+      span2.setAttribute('class', 'subject');
+      span3.setAttribute('class', 'body');
+      span1.textContent = 'tunde@mail.com';
+      span2.textContent = `${message.subject} -`
+      // shorten message to 100 characters
+      const shortendMessage = message.message.substring(0, 50);
+      span3.textContent = `${shortendMessage}...`;
+      li.appendChild(span1);
+      li.appendChild(span2);
+      li.appendChild(span3);
+      ul.appendChild(li);
+    });
+    mainSection.appendChild(ul);
   })
   .catch((error) => console.log(error));
