34ad3c789a5c2ea68b59f2648cdc14ea9f771cba
Tunde Yakub <tundenasri@gmail.com>
2019-04-17 11:58:39 +0100

    fetch email by id

--- Ui/js/view-email.js
+++ Ui/js/view-email.js
@@ -1,2 +1,14 @@
+const messageId = localStorage.getItem('messageId');
 
-const messageId = localStorage.getItem('messageId')
+const url = `https://intense-thicket-60071.herokuapp.com/api/v1/messages/${messageId}`;
+const headers = new Headers();
+headers.append('Content-Type', 'application/json');
+headers.append('authorization', localStorage.getItem('token'));
+
+fetch(url, {
+  method: 'GET',
+  headers,
+})
+  .then((response) => response.json())
+  .then((res) => console.log(res))
+  .catch((error) => console.log(error));
