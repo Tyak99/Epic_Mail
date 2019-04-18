a47e29355f1d19b74e537e0809e6454a3ad4a065
Tunde Yakub <tundenasri@gmail.com>
2019-04-18 15:43:26 +0100

    add modal to display feedback in create group page

--- Ui/js/group.js
+++ Ui/js/group.js
@@ -1,4 +1,17 @@
 const submitButton = document.querySelector('.submit-btn');
+const modal = document.getElementById('id-modal');
+const modalContent = document.querySelector('.modal-content');
+const modalMessage = document.getElementById('modal-message');
+const closeModalButton = document.querySelector('.close-button');
+const okayModalButton = document.querySelector('.yes-modal');
+
+// modal for displaying feedback
+const toggleModal = (message, result) => {
+  modalMessage.innerHTML = message;
+  modalContent.style.backgroundColor =
+    result === 'success' ? '#388438' : '#d25353';
+  modal.classList.toggle('show-modal');
+};
 
 const url = 'https://intense-thicket-60071.herokuapp.com/api/v1/groups';
 const headers = new Headers();
@@ -11,14 +24,27 @@ const createGroup = (e) => {
     name: groupName,
   });
   e.preventDefault();
+  if (groupName.length < 2) {
+    toggleModal('Group name should be at least 2 characters long', 'failed');
+    return;
+  }
   fetch(url, {
     method: 'POST',
     headers,
     body: data,
   })
     .then((response) => response.json())
-    .then((res) => console.log(res))
+    .then((res) => {
+      if (res.status == 'failed') {
+        toggleModal(`Error! ${res.error}`, 'failed');
+      }
+      if (res.status === 'success') {
+        toggleModal('Group successfully created', 'success');
+      }
+    })
     .catch((error) => console.log(error));
 };
 
 submitButton.addEventListener('click', createGroup);
+closeModalButton.addEventListener('click', toggleModal);
+okayModalButton.addEventListener('click', toggleModal);
