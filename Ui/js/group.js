b2cd4df3bd8cc4566173f6d50d844e0a97c13ef2
Tunde Yakub <tundenasri@gmail.com>
2019-04-20 15:53:50 +0100

    remove modal for create group alerts

--- Ui/js/group.js
+++ Ui/js/group.js
@@ -3,7 +3,7 @@ const modal = document.getElementById('id-modal');
 const modalContent = document.querySelector('.modal-content');
 const modalMessage = document.getElementById('modal-message');
 const closeModalButton = document.querySelector('.close-button');
-const okayModalButton = document.querySelector('.yes-modal');
+// const okayModalButton = document.querySelector('.yes-modal');
 const listGroups = document.querySelector('.list-groups');
 
 // header for sending requests
@@ -13,10 +13,7 @@ headers.append('Content-Type', 'application/json');
 headers.append('authorization', localStorage.getItem('token'));
 
 // modal for displaying feedback
-const toggleModal = (message, result) => {
-  modalMessage.innerHTML = message;
-  modalContent.style.backgroundColor =
-    result === 'success' ? '#388438' : '#d25353';
+const toggleModal = () => {
   modal.classList.toggle('show-modal');
 };
 
@@ -39,7 +36,8 @@ const createGroup = (e) => {
   });
   e.preventDefault();
   if (groupName.length < 2) {
-    toggleModal('group name should be at least 2 characters long', 'failed');
+    // toggleModal('group name should be at least 2 characters long', 'failed');
+    console.log('group name should be at least 2 characters long')
     return;
   }
   fetch(url, {
@@ -50,7 +48,8 @@ const createGroup = (e) => {
     .then((response) => response.json())
     .then((res) => {
       if (res.status == 'failed') {
-        toggleModal(`Error! ${res.error}`, 'failed');
+        // toggleModal(`Error! ${res.error}`, 'failed');
+        console.log('failed')
       }
       if (res.status === 'success') {
         document.getElementById('groupname').value = '';
@@ -88,4 +87,4 @@ fetch(url, {
 
 submitButton.addEventListener('click', createGroup);
 closeModalButton.addEventListener('click', toggleModal);
-okayModalButton.addEventListener('click', toggleModal);
+// okayModalButton.addEventListener('click', toggleModal);
